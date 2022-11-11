e233e38e8de94c7a0f3b7f1d024697a034725fb2
yuanpengfei03 <yuanpengfei03@meituan.com>
2022-11-11 19:27:17 +0800

    jsdom add updatetime and createtime and prev and next

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,21 @@ const fs = require("fs-extra");
 const Path = require("path");
 const Util = require("util");
 const ejs = require("ejs");
+const JSDOM = require("jsdom").JSDOM;
 const minify = require("html-minifier").minify;
 const execSync = require("child_process").execSync;
+const EventEmitter = require("events").EventEmitter;
+const dayjs = require("dayjs");
 const config = require("../config.json");
 const convert = new showdown.Converter();
 const readFileAsync = Util.promisify(fs.readFile);
 
 const resolvePath = (path, mid = "../") => Path.resolve(__dirname, mid, path);
+
 const isDir = (path) => fs.statSync(path).isDirectory();
+
+const getAllArticleCount = (path = "./") =>
+  Number(execSync(`cd ${path} && ls -lR|grep "^-"|wc -l`).toString());
 
 const categoryMap = {};
 
@@ -25,53 +32,144 @@ const minifyConfig = {
 
 const entry = resolvePath(config.entry);
 const output = resolvePath(config.output);
+const count = getAllArticleCount(entry);
 
 async function doConvert() {
+  console.log("转换开始啦");
   const templatePath = resolvePath(Path.resolve(__dirname, "./template.html"));
+
   if (!fs.existsSync(output)) {
     fs.ensureDirSync(output);
   } else {
     execSync(`rm -rf ${output}/*`);
   }
 
+  let _count = 0;
+  const event = new EventEmitter();
+
+  async function genHomePage() {
+    const res = await ejs.renderFile(Path.resolve(__dirname, "./index.html"), {
+      title: config.name,
+      content: categoryMap,
+      github: config.github,
+    });
+    await fs.writeFile(`${output}/index.html`, minify(res, minifyConfig));
+    console.log("转换搞定啦");
+  }
+
+  event.on("done", genHomePage);
+
+  async function genHtmlTxt(path) {
+    const res = await readFileAsync(path);
+    return convert.makeHtml(res.toString());
+  }
+  // data-time="<%= updateDate %>|<%= createDate %>"
+  function genArticlePath(path) {
+    const articlePath = Path.parse(path.replace("md", "html"));
+    articlePath.dir = articlePath.dir.replace(config.entry, config.output);
+    return articlePath;
+  }
+
+  function setCategoryMap(path) {
+    const category = path.dir.split(`${config.output}/`)[1];
+    if (!categoryMap[category]) {
+      categoryMap[category] = [];
+    }
+    if (!categoryMap[category].includes(path.base)) {
+      categoryMap[category].push(path.base);
+    }
+    return [category, path.base];
+  }
+
+  async function genArticleContent(path, opts = {}) {
+    return await ejs.renderFile(templatePath, {
+      title: path.name,
+      ...opts,
+      name: config.name,
+      github: config.github,
+    });
+  }
+
+  function ensureArticlePath(path) {
+    if (!fs.existsSync(path.dir)) {
+      fs.ensureDirSync(path.dir);
+    }
+  }
+
+  function formatTime(date) {
+    return dayjs(date).format(config.datetime.format || "YYYY/MM/DD HH:mm:ss");
+  }
+
+  function getPrevOrNext(entry, path) {
+    if (path) {
+      const tempPath = Path.resolve(entry, path);
+      const articlePath = genArticlePath(tempPath);
+      const [c, p] = setCategoryMap(articlePath);
+      return `/${c}/${p}`;
+    }
+
+    return "";
+  }
+
   async function read2Cov(entry) {
     const _paths = await fs.readdir(entry);
-    for (const _path of _paths) {
+    for (let index = 0; index < _paths.length; index++) {
+      const _path = _paths[index];
       const tempPath = Path.resolve(entry, _path);
       if (!isDir(tempPath)) {
-        const res = await readFileAsync(tempPath);
-        const htmlTxt = convert.makeHtml(res.toString());
-        const articlePath = Path.parse(tempPath.replace("md", "html"));
-        articlePath.dir = articlePath.dir.replace(config.entry, config.output);
-        const ca = articlePath.dir.split(`${config.output}/`)[1];
-        if (!categoryMap[ca]) {
-          categoryMap[ca] = [];
+        const stats = fs.statSync(tempPath);
+        const updateDate = formatTime(stats.mtime); // 更新修改时间
+        const createDate = formatTime(stats.birthtime); // 创建时间
+
+        let htmlTxt = await genHtmlTxt(tempPath);
+        if (config.datetime && config.datetime.use) {
+          const dom = new JSDOM(htmlTxt);
+          const header = dom.window._document.querySelector("h1");
+          header.setAttribute(
+            "data-time",
+            "更新时间：<%= updateDate %> | 创建时间： <%= createDate %>"
+          );
+
+          htmlTxt = dom.serialize();
         }
-        categoryMap[ca] = [articlePath.base];
-        if (!fs.existsSync(articlePath.dir)) {
-          fs.ensureDirSync(articlePath.dir);
+        const articlePath = genArticlePath(tempPath);
+
+        setCategoryMap(articlePath);
+
+        ensureArticlePath(articlePath);
+
+        let prev, next;
+
+        if (_paths[index + 1]) {
+          next = getPrevOrNext(entry, _paths[index + 1]);
         }
-        const content = await ejs.renderFile(templatePath, {
-          title: articlePath.name,
+
+        if (_paths[index - 1]) {
+          prev = getPrevOrNext(entry, _paths[index - 1]);
+        }
+
+        const content = (
+          await genArticleContent(articlePath, { prev, next })
+        ).replace("{content}", htmlTxt);
+
+        const txt = ejs.render(content, {
+          updateDate,
+          createDate,
         });
-        const minified = minify(
-          content.replace("{content}", htmlTxt),
-          minifyConfig
-        );
+        const minified = minify(txt, minifyConfig);
+
         await fs.writeFile(Path.format(articlePath), minified);
+        _count++;
+        if (_count === count) {
+          event.emit("done");
+        }
       } else {
-        await read2Cov(tempPath);
+        read2Cov(tempPath);
       }
     }
   }
 
-  await read2Cov(entry);
-  const res = await ejs.renderFile(Path.resolve(__dirname, "./index.html"), {
-    title: config.name,
-    content: categoryMap,
-    github: config.github,
-  });
-  await fs.writeFile(`${output}/index.html`, minify(res, minifyConfig));
+  const res = await read2Cov(entry);
 }
 
 doConvert();
